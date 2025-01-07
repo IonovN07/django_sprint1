b656670adf8fe25f7c278f023ac1676da8f5f8ae
Nikita Ionov <nhjase@yandex.ru>
2025-01-07 21:58:51 +0500

    Add final changes

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -19,9 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('blog.urls')),
-    # path('post/', include('blog.urls')),
-    # path('category/', include('blog.urls')),
-    
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
