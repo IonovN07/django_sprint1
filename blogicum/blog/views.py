30ccc8cc90e07ef310cb9e66746e8abc3a269936
Nikita Ionov <nhjase@yandex.ru>
2025-01-08 22:27:33 +0500

    Add guard blok in blog view

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import render
 
 posts = [
     {
@@ -44,20 +44,18 @@ posts = [
     },
 ]
 
+posts_id = dict((post['id'], post) for post in posts)
+
 
 def index(request):
-    context = {'posts': posts}
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def post_detail(request, post_id):
-    public = None
-    for post in posts:
-        if post['id'] == post_id:
-            public = post
-    if not public:
-        raise Http404
-    return render(request, 'blog/detail.html', {'post': public})
+    if post_id not in posts_id:
+        raise Http404('Запрашиваемая вами страница не найдена')
+    post = posts_id[post_id]
+    return render(request, 'blog/detail.html', {'post': post})
 
 
 def category_posts(request, category_slug):
